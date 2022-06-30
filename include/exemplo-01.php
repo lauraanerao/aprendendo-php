9ef7b2069b2cc33e1398ac3fef7cb7bb258bea1a
Laura Anerão <lauralins@gmail.com>
2022-06-29 22:08:40 -0500

    Aula do dia 29/06/2022 - require e include.

---
+++ include/exemplo-01.php
@@ -0,0 +1,8 @@
+<?php
+var_dump("OK");
+
+function somar($a, $b){
+
+    return $a + $b;
+
+}
---
+++ include/exemplo-02.php
@@ -0,0 +1,7 @@
+<?php
+//include "exemplo-01.php";
+require"exemplo-01.php";
+
+$resultado = somar(10, 20);
+
+echo $resultado;
