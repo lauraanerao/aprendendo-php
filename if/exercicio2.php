7efb821207e08b64a9678df7641ad4e7ab78c5ba
Laura Anerão <lauralins@gmail.com>
2022-07-09 11:28:50 -0500

    Exercício 1

---
+++ if/exercicio2.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+ * Site exercícios: https://satellasoft.com/artigo/php/lista-de-exercicios-em-php
+ */
+
+/*
+ * Crie um algoritmo que receba um número digitado pelo usuário. Verifique se esse valor é positivo, negativo
+ * ou igual a zero. A saída deve ser: "Valor Positivo", "Valor Negativo" ou "Igual a Zero".
+ */
+
+//$numero = $_GET['numero'];
+//if ($numero > 0) {
+//
+//    echo ("Valor Positivo");
+//}
+//
+//elseif ($numero < 0) {
+//
+//    echo ("Valor Negativo");
+//}
+//
+//else {
+//
+//    echo ("Igual a Zero");
+//}
+
+/*
+ * Faça um algoritmo PHP que receba os valores A e B. Imprima-os em ordem crescente.
+ * Exemplo: Para A=5, B=4, imprima "4 5".
+ */
+
+/*
+ * Faça um algoritmo em PHP que verifica se o valor da variável A é maior ou menor que o valor da variável B.
+ * A mensagem a ser impressa deve ser “A maior que B” ou “A menor que B”.
+ */
+
+/*
+ * Crie um algoritmo que pergunte ao usuário seu nome e sua idade. Verifique se a idade é maior ou menor que 18.
+ * Exiba da seguinte forma: $nome é maior/menor de idade e tem $idade anos
+ */
+
+/*
+ * Bonus round: Ler um número inteiro entre 1 e 12 e escrever o mês correspondente.
+ * Caso o número seja fora desse intervalo, informar que não existe mês com este número.
+ */
+
+$mes = [
+
+];
+
