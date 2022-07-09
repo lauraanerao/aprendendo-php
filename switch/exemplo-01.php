24c344ef2d844bbed08dacf4f087b709f431434b
Laura Anerão <lauralins@gmail.com>
2022-07-09 12:12:46 -0500

    Aula Switch - 09/07/2022.

---
+++ switch/exemplo-01.php
@@ -0,0 +1,37 @@
+<?php
+
+$diaDaSemana = 8; //date("w");
+
+switch ($diaDaSemana) {
+
+    case 0:
+        echo "Domingo";
+        break;
+
+    case 1:
+        echo "Segunda-feira";
+        break;
+
+    case 2:
+        echo "Terça-feira";
+        break;
+
+    case 3:
+        echo "Quarta-feira";
+        break;
+
+    case 4:
+        echo "Quinta-feira";
+        break;
+
+    case 5:
+        echo "Sexta-feira";
+        break;
+
+    case 6:
+        echo "Sábado";
+        break;
+
+    default:
+        echo "Data Inválida";
+}
