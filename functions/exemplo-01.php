36617f69670452f81569334296d26a6a6f69e6df
Laura Anerão <lauralins@gmail.com>
2022-07-23 13:05:45 -0500

    Aula de funções. Dia 23/07/2022.

---
+++ functions/exemplo-01.php
@@ -0,0 +1,12 @@
+<?php
+
+function ola(){
+
+    return "Olá, Mundo!<br>";
+}
+
+echo ola();
+
+$frase = ola();
+
+echo strlen($frase);
---
+++ functions/exemplo-02.php
@@ -0,0 +1,9 @@
+<?php
+
+function salario(){
+
+    return 946.00;
+
+}
+
+echo "José recebeu três salários:" . (salario()*3);
