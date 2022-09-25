5ec7a24c89e6872341ac5611d626d0eed944704b
Laura Anerão <lauralins@gmail.com>
2022-09-24 22:32:34 -0500

    Aula de usando imagem com PhP. Dia 24/09/2022.

---
+++ gd/exemplo-01.php
@@ -0,0 +1,14 @@
+<?php
+
+header("Content-Type: image/png");
+
+$image = imagecreate(256, 256);
+
+$black = imagecolorallocate($image, 0, 0, 0);
+$red = imagecolorallocate($image, 255, 0, 0);
+
+imagestring($image, 5, 60, 120, "Hello World", $red);
+
+imagepng($image);
+
+imagedestroy($image);
